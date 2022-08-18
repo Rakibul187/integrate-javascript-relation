078e5388b6ad66ef82cedf16f66fa86d573656a3
Rakibul Islam <rakibul241221@gmail.com>
2022-08-18 14:32:38 +0600

    count by event listener

--- first-conceptual-dom-events/counter.js
+++ first-conceptual-dom-events/counter.js
@@ -1,4 +1,18 @@
 let count = 0;
+
+// by event handler
+function counter() {
+    count++;
+    document.getElementById('count').innerText = count;
+}
+
+function counter2() {
+    count--;
+    document.getElementById('count').innerText = count;
+}
+
+/*
+ // by event listener
 document.getElementById('plus-btn').addEventListener('click', function () {
     count = count + 1;
     document.getElementById('count').innerText = count;
@@ -7,4 +21,5 @@ document.getElementById('plus-btn').addEventListener('click', function () {
 document.getElementById('minus-btn').addEventListener('click', function () {
     count = count - 1;
     document.getElementById('count').innerText = count;
-})
+}) 
+*/
